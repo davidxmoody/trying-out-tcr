628973de562e5138618afce36f9df4356475ae07
David Moody <david@davidxmoody.com>
2019-01-05 11:27:25 +0000

    TCR

--- src/test.ts
+++ src/test.ts
@@ -2,7 +2,11 @@ import {expect} from "chai"
 import fun from "./index"
 
 describe("test", () => {
-  it("returns true", () => {
+  it("returns true if the target is contained in the list", () => {
     expect(fun(1, [1])).to.be.true
+  })
+
+  it("returns false if the target is not contained in the list", () => {
+    expect(fun(1, [2])).to.be.false
   })
 })
