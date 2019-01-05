a5a017eb8cfb041a1baa1220d59e391bef175616
David Moody <david@davidxmoody.com>
2019-01-05 11:42:54 +0000

    TCR

--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,16 @@
 import {expect} from "chai"
 import fun from "./index"
 
+const testCases: Array<[number, number[], boolean]> = [
+  [1, [1], true],
+  [1, [2], false],
+  [3, [1, 2], true],
+]
+
 describe("test", () => {
-  it("returns true if the target is contained in the list", () => {
-    expect(fun(1, [1])).to.be.true
-  })
-
-  it("returns false if the target is not contained in the list", () => {
-    expect(fun(1, [2])).to.be.false
-  })
-
-  it("returns true if the numbers can be added to create the target", () => {
-    expect(fun(3, [1, 2])).to.be.true
-  })
+  for (const testCase of testCases) {
+    it(JSON.stringify(testCase), () => {
+      expect(fun(testCase[0], testCase[1])).to.eql(testCase[2])
+    })
+  }
 })
