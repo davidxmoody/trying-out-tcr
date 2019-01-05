9528de3827ee33a908537d43225a8efdd0bc0503
David Moody <david@davidxmoody.com>
2019-01-05 11:51:19 +0000

    TCR

--- src/test.ts
+++ src/test.ts
@@ -8,6 +8,7 @@ const testCases: Array<[number, number[], boolean]> = [
   [3, [1, 2, 1], true],
   [3, [9, 2, 1], true],
   [3, [1, 2, 9], true],
+  [3, [2, 9], false],
 ]
 
 describe("Countdown game", () => {
