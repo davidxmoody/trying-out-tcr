91fa5452ee2ecc1967567132b6d6386060a3da18
David Moody <david@davidxmoody.com>
2019-01-05 12:25:16 +0000

    TCR

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,17 @@ const operators: Array<(a: number, b: number) => number> = [
   (a, b) => b / a,
 ]
 
+function extract(nums: number[], i: number, j: number): [number, number, number[]] {
+  if (i < 0 || j < 0 || i >= nums.length || j >= nums.length || i >= j) {
+    throw new Error(`Preconditions not met, ${nums}, ${i}, ${j}`)
+  }
+
+  const clone = [...nums]
+  const b = clone.splice(j, 1)[0]
+  const a = clone.splice(i, 1)[0]
+  return [a, b, clone]
+}
+
 function test(target: number, nums: number[]): boolean {
   if (nums.length === 0) {
     throw new Error("No nums left, should never reach this point")
@@ -18,13 +29,9 @@ function test(target: number, nums: number[]): boolean {
     return nums[0] === target
   }
 
-  const i = 0
-  const j = 1
-  const numsClone = [...nums]
-  const b = numsClone.splice(j, 1)[0]
-  const a = numsClone.splice(i, 1)[0]
+  const [a, b, rest] = extract(nums, 0, 1)
 
-  return operators.some(op => test(target, [op(a, b), ...numsClone]))
+  return operators.some(op => test(target, [op(a, b), ...rest]))
 }
 
 export default test
